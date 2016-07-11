d132189b9ffe1f473aeb6dbccfd21687cb3aae8a
osman akbacı <akbaciosman@users.noreply.github.com>
2016-07-11 23:18:46 +0300

    Delete received_message.sh

--- received_message.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-while true;do
-	PACKET=$(nc -l 10003)
-	NICK=$(echo $PACKET | cut -d ',' -f1)
-	MESSAGE=$(echo $PACKET | cut -d ',' -f2)
-
-	echo "NickName : $NICK Message : $MESSAGE"
-done
