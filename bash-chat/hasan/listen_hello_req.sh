3ba5df302e6242faec295a7a133613941eee2426
alppboz <alppboz@gmail.com>
2016-07-12 12:00:21 +0300

    Update listen_hello_req.sh

--- bash-chat/hasan/listen_hello_req.sh
+++ bash-chat/hasan/listen_hello_req.sh
@@ -2,7 +2,7 @@ while true; do
 PACKET=$(nc -l 10000)
 IPADDR=$(echo $PACKET | cut -d ',' -f1)
 NICK=$(echo $PACKET | cut -d ',' -f2)
-echo "172.16.5.194,hasan" | nc -w 1 $IPADDR 10001 &
+echo "172.16.5.194,hasan" | nc -w 1 $IPADDR 10001
 if grep -cq "$IPADDR" ip_nick.txt; 
 then
      :
