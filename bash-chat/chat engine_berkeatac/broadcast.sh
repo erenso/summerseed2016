a018fd371029bf472b16e4b56362fca8882043f0
Berke Atac <berkeatac@sabanciuniv.edu>
2016-07-12 11:03:46 +0300

    Revert "Add files via upload"
    This reverts commit 07bfe7a2d476826addd127a4e2ef5f40f42718f8.

--- bash-chat/chat engine_berkeatac/broadcast.sh
+++
@@ -1,6 +0,0 @@
-read MSG
-for i in {2..254}
-do
-   echo "berke,$MSG" | nc -c 172.16.5.$i 10002 &
-done
-echo "console: message sent to all"
--- bash-chat/chat engine_berkeatac/discover.sh
+++
@@ -1,5 +0,0 @@
-for i in {2..254}
-do
-   echo "172.16.5.203,berke" | nc -c 172.16.5.$i 10000 &
-   echo "$i"
-done
